06f69de24b721191b9a1bf4fd21f2322a71f4254
Ani Agajanian <aganita21@gmail.com>
2015-10-22 19:48:23 -0400

    Added Kadanes and Sieve of Eratosthenes algorithms

--- commonAlgorithms.js
+++ commonAlgorithms.js
@@ -1,4 +1,6 @@
-// sort numbers in reverse order
+// sort numbers 
+// By default, the sort() method sorts the values as strings in alphabetical and ascending order. 
+
 var arr = [5, 2, 1, -10, 8];
 
 function compare(a, b) {
@@ -13,4 +15,51 @@ alert( arr.sort(compareReversed)) ;
 
 	
 	
-//	
+//get the sum of the Max sub array - Kadane's algorithm
+function getMaxSubSum(arr) {
+  var maxSum = 0,
+    partialSum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    partialSum += arr[i];
+    maxSum = Math.max(maxSum, partialSum);
+    if (partialSum < 0) partialSum = 0;
+  }
+  return maxSum;
+}
+
+alert (getMaxSubSum(arr));
+
+
+//find the prime numbers - Sieve of Eratosthenes algorithm 
+// step one - create an array with true values
+var arr = [];
+
+for (var i = 2; i < 100; i++) {
+  arr[i] = true
+}
+
+// step 2 - start with the first prime number
+var p = 2;
+
+do {
+  // step 3 - eliminate all the numbers that are multiples of the chosen prime number 
+  for (i = 2 * p; i < 100; i += p) {
+    arr[i] = false;
+  }
+
+  // step 4 - find the next available prime number
+  for (i = p + 1; i < 100; i++) {
+    if (arr[i]) break;
+  }
+
+  p = i;
+} while (p * p < 100); // step 5 - keep going till the square of prime number is inside the array
+
+// save the prime numbers in the string then show them in alert
+var printPrimeNum = "";
+for (var j = 2; j < 10; j++) {
+	if (arr[j]) {
+		printPrimeNum = printPrimeNum + j + ", ";
+	}
+}
+alert (printPrimeNum);
