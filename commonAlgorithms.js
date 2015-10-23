b542faa617cee983dddce658498151db23814892
Ani Agajanian <aganita21@gmail.com>
2015-10-23 17:17:41 -0400

    Added a filter algorithm

--- commonAlgorithms.js
+++ commonAlgorithms.js
@@ -185,4 +185,20 @@ alert (printPrimeNum);
 
 
 
+//Filter the array by given range 
 
+function filterRangeInPlace(arr, a, b) {
+
+  for (var i = 0; i < arr.length; i++) {
+    var val = arr[i];
+    if (val < a || val > b) {
+      arr.splice(i--, 1);
+    }
+  }
+
+}
+
+var arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4);
+alert( arr ); // [3, 1]
