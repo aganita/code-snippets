051b597a5bb57e673a38e6d051011b2de70d04ed
Ani <aganita21@gmail.com>
2015-12-20 16:29:07 -0500

    updated the intermediate algorithms

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -14,26 +14,26 @@ sumAll([5, 10]);
 //DIFF TWO ARRAYS
 //Compare two arrays and return a new array with 
 //any items only found in one of the original arrays.
-
 function diff(arr1, arr2) {
   var newArr = [];
   
- for (var i = 0; i < arr1.length; i++) {
+  for (var i = 0; i < arr1.length; i++) {
     if (arr2.indexOf(arr1[i]) < 0){
-        newArr.push(arr1[i]);
+      newArr.push(arr1[i]);
     } 
- }
+  }
 
- for (var j = 0; j < arr2.length; j++) {
+  for (var j = 0; j < arr2.length; j++) {
     if (arr1.indexOf(arr2[j]) < 0){
-        newArr.push(arr2[j]);
+      newArr.push(arr2[j]);
     } 
- }
+  }
   
   return newArr;
 }
 
 diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 diff(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
+
 
 
