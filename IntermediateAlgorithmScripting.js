74746144f581d9480bd99d82c8eab100d4edea0a
Ani <aganita21@gmail.com>
2015-12-20 13:56:37 -0500

    Create IntermediateAlgorithmScripting.js

---
+++ IntermediateAlgorithmScripting.js
@@ -0,0 +1,13 @@
+//SUM ALL NUMBERS IN A RANGE
+//We'll pass you an array of two numbers. 
+//Return the sum of those two numbers and all numbers between them.
+
+
+function sumAll(arr) {
+  var sum = 0;
+  for (var i = Math.min(...arr); i<= Math.max(...arr); i++)
+    sum += i;  
+  return sum;
+}
+
+sumAll([5, 10]);
