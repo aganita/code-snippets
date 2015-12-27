47f34c4c8acd728e95aaf3d7adb39e641e607550
Ani <aganita21@gmail.com>
2015-12-26 21:05:04 -0500

    Update intermediateAlgorithmScripting.js

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -259,3 +259,15 @@ function fearNotLetter(str) {
     return strArr.join("");
 }
 fearNotLetter("abcefh");
+
+
+// BOO WHO
+//Check if a value is classified as a boolean primitive. Return true or false.
+//Boolean primitives are true and false.
+
+function boo(bool) {
+  return typeof bool === 'boolean';
+}
+
+boo(null);
+
