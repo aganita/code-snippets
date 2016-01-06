402f04d62a3af73e8c95fd6a47bb51355d697b91
Ani <aganita21@gmail.com>
2016-01-05 23:34:10 -0500

    added new examples

--- commonAlgorithms.js
+++ commonAlgorithms.js
@@ -338,6 +338,23 @@ function sum(a) {
 alert( sum(1)(2) );
 alert( sum(5)(-1) );
 
+// get sum, generic version
+function sum(a) {
+  var currentSum = a;
+  function f(b) {
+    currentSum += b;
+    return f;
+  }
+  f.toString = function() {
+    return currentSum;
+  };
+  return f;
+}
+alert( sum(1)(2) ); // 3
+alert( sum(5)(-1)(2) ); // 6
+alert( sum(6)(-1)(-2)(-3) ); // 0
+alert( sum(0)(1)(2)(3)(4)(5) ); // 15
+
 
 ////// fun facts about objects ////////
 var obj = {
