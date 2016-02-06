2e946a57c062f34824b7ff0c7d4f6fff694027d8
Ani <aganita21@gmail.com>
2016-02-06 14:22:01 -0500

    implementation of try catch finally

---
+++ tryCatch.js
@@ -0,0 +1,36 @@
+function ReadError(message, cause) {
+  this.message = message;
+  this.cause = cause;
+  this.name = 'ReadError';
+  this.stack = cause.stack;
+}
+
+function readData() {
+  var data = '{ bad data }';
+
+  try {
+    // ...
+    JSON.parse(data);
+    // ...
+  } catch (e) {
+    // ...
+    if (e.name == 'URIError') {
+      throw new ReadError("URI Error", e);
+    } else if (e.name == 'SyntaxError') {
+      throw new ReadError("Data Syntax Error", e);
+    } else {
+      throw e; // пробрасываем
+    }
+  }
+}
+
+try {
+  readData();
+} catch (e) {
+  if (e.name == 'ReadError') {
+    alert( e.message );
+    alert( e.cause ); // the original cause of the error 
+  } else {
+    throw e;
+  }
+}
