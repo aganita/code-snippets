d671db03fa3cb15f6a39ddc444511f96d9bdf45e
Ani <aganita21@gmail.com>
2016-02-19 16:29:34 -0500

    implementation of Stack

--- implementationOfDA.js
+++ implementationOfDA.js
@@ -38,3 +38,72 @@ myStack.push("3");
 myStack.push(4);
 myStack.push(5);
 myStack.print();onsole.log(myStack.print());
+
+
+
+// create a list class in JavaScript
+class LinkedList{
+    constructor() {
+        this._head = null;
+        this._length = 0;
+    };
+    
+    add(val){
+        var node = {
+            value: val,
+            next: null
+        };
+
+        if(this._head === null){
+            this._head = node; 
+        }
+        else{
+            let current = this._head;
+            while(current.next){
+                current = current.next;
+            }
+            current.next = node;
+            this._length++;
+        }
+    }
+    
+    print(){
+        let current = this._head;
+        while (current) {
+            console.log( current.value );
+            current = current.next;
+        }
+    }
+    
+    remove(val){
+        let current = this._head;
+        //case-1 
+        if(current.value == val){
+            this._head = current.next;     
+        }
+        else{
+            let previous = current;
+            while(current.next){
+        //case-3
+        if(current.value == val){
+          previous.next = current.next;          
+          break;
+        }
+        previous = current;
+        current = current.next;
+      }
+      //case -2
+      if(current.value == val){
+        previous.next == null;
+      }
+    }
+    }
+}
+
+var smth = new LinkedList();
+smth.add(5);
+smth.add(6);
+smth.add(5);
+smth.remove(5);
+smth.print();
+
